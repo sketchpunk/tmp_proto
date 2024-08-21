45c95ad8368579f9623e2d8e4e837bab4ff8ff8b
Pedro Sousa <sketchpunk@gmail.com>
2024-08-21 15:51:00 -0400

    fix paint op

--- painter/OpPaintBrush.js
+++ painter/OpPaintBrush.js
@@ -5,6 +5,7 @@ export default class PaintBrushOp{
     editor      = null;
     layer       = null;
     isDown      = false;
+    prevPos     = null;
     constructor( editor ){ this.editor = editor; }
     // #endregion
 
@@ -12,7 +13,9 @@ export default class PaintBrushOp{
     onInit(_obj){ this.layer = this.editor.layers.pixel; }
 
     onRelease(_obj){
-        this.layer.clearPreview().setErase( false );
+        this.layer
+            .clearPreview()
+            .setErase( false );
         this.layer = null;
     }
 
@@ -33,13 +36,17 @@ export default class PaintBrushOp{
             .clearPreview();
 
         this.paintBrush( this.layer.ctxDraw, x, y );
-        this.isDown = true;
+        this.prevPos    = [x,y];
+        this.isDown     = true;
         return true;
     }
 
     onPointerMove( x, y, e, obj ){
         if( this.isDown ){
-            this.paintBrush( this.layer.ctxDraw, x, y );
+            this.paintMove( this.layer.ctxDraw, x, y );
+
+            this.prevPos[0] = x;
+            this.prevPos[1] = y;
         }else{
             this.layer.clearPreview();
             this.paintBrush( this.layer.ctxPreview, x, y );
@@ -47,11 +54,12 @@ export default class PaintBrushOp{
     }
 
     onPointerUp( _e, _obj ){
-        
-        this.isDown = false;
+        this.prevPos    = null;
+        this.isDown     = false;
     }
-    onPointerCancel( _e, _obj ){ 
-        this.isDown = false;
+    onPointerCancel( _e, _obj ){
+        this.prevPos    = null;
+        this.isDown     = false;
     }
     onDblClick( _e, _obj ){ return false; }
     // #endregion
@@ -74,6 +82,29 @@ export default class PaintBrushOp{
                     size, size
                 );
                 break;
+        }
+    }
+
+    paintMove( ctx, x, y ){
+        // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        const px    = this.prevPos[0];              // Previous Position
+        const py    = this.prevPos[1];
+        const dx    = x - px;                       // Delta Position
+        const dy    = y - py;
+        const dist  = Math.sqrt( dx**2 + dy**2 );   // Distance traveled
+
+        // How many brush steps at half brush size
+        const inc   = Math.max( 1, Math.round( this.editor.painting.size * 0.5 ) );
+        const steps = Math.max( 1, Math.ceil( dist / inc ) );
+        
+        // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        let t;
+        for( let i=1; i <= steps; i++ ){
+            t = i / steps;
+            this.paintBrush( ctx, 
+                Math.round( dx * t + px ), 
+                Math.round( dy * t + py )
+            );
         }
     }
     // #endregion
